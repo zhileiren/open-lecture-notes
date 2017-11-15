92016f05fb3365cbb764ba59b1db8aee6d2f0e70
Zhilei Ren <zren@dlut.edu.cn>
2017-11-15 19:53:50 +0800

    tree

---
+++ part1/lecture4/tree.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import turtle
+import canvasvg
+
+def tree(len, n):
+    if n <= 0:
+        return
+    else:
+        turtle.forward(len)
+        turtle.left(45)
+        tree(len * 0.5, n - 1)
+        turtle.right(90)
+        tree(len * 0.5, n - 1)
+        turtle.left(45)
+        turtle.backward(len)
+
+turtle.speed(0)
+tree(100, 9)
+turtle.hideturtle()
+ts = turtle.getscreen().getcanvas()
+canvasvg.saveall("tree.svg", ts)
