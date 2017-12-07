5c0097badb42561009b50dbd1aaba3e1b19d9005
Zhilei Ren <zren@dlut.edu.cn>
2017-12-07 22:43:38 +0800

    lec9

---
+++ part2/lecture9/a.sql
@@ -0,0 +1,3 @@
+create table news (id integer, score integer, title text, href text);
+insert into news values (1, 8, "hello world", "http://oscar-lab.org");
+insert into news values (2, 2, "hello charlie", "http://www.dlut.edu.cn");
