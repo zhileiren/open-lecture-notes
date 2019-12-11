3566d65a638b0cf9e48f4c7de4ca055cf1a106f7
Zhilei Ren <zren@dlut.edu.cn>
2019-12-12 01:12:53 +0800

    triangle

--- part3/lecture11/testTriangle.py
+++ part3/lecture11/testTriangle.py
@@ -10,5 +10,7 @@ class TestTriangle(unittest.TestCase):
     def test_triangle2(self):
         self.assertEqual(triangle(1, 1, 1), "等边")
     def test_triangle3(self):
-        self.assertEqual(triangle(2, 2, 3), "等边")
+        self.assertEqual(triangle(1, 3, 2), "错误")
 
+if __name__ == "__main__":
+    unittest.main()
--- part3/lecture11/triangle.py
+++ part3/lecture11/triangle.py
@@ -1,18 +1,24 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#!/usr/bin/python3
+# coding: utf-8
 
 def triangle(a, b, c):
-    if a + b <= c or a + c <= b or b + c <= a:
+    # a, b, c = sorted([a, b, c])
+    if a + b <= c:
         return "错误"
+    triangle_type = ""
+    if a == b == c:
+        triangle_type = "等边"
+    elif a == b or b == c or a == c:
+        triangle_type = "等腰"
     else:
-        if a == b == c:
-            return "等边"
-        elif a != b != c:
-            return "其他"
-        else:
-            return "等边"
+        triangle_type = "一般"
+
+    if a ** 2 + b ** 2 == c ** 2:
+        triangle_type += "直角"
+    return triangle_type
 
 if __name__ == "__main__":
-    print(triangle(0, 0, 0))
     print(triangle(1, 1, 1))
-    print(triangle(2, 2, 3))
+    print(triangle(1, 2, 3))
+    print(triangle(3, 3, 3))
+    print(triangle(3, 4, 5))
